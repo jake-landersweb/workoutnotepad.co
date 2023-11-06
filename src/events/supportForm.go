1249ec49ee67f0a1af50cb1391e0f6641138c3c1
jake-landersweb <jake@landersweb.com>
2023-11-06 12:54:00 -0800

    error when sending form

--- src/events/supportForm.go
+++ src/events/supportForm.go
@@ -50,10 +50,15 @@ func EventSupportForm(w http.ResponseWriter, r *http.Request) (string, int) {
 
 	encoded, _ := json.Marshal(data)
 
-	resp, _ := http.Post("https://pocketbase.sapphirenw.com/api/collections/wn_feedback_v2/records", "application/json", bytes.NewBuffer(encoded))
+	resp, err := http.Post("https://pocketbase.sapphirenw.com/api/collections/wn_feedback_v2/records", "application/json", bytes.NewBuffer(encoded))
+	if err != nil {
+		fmt.Printf("Error sending request: %s\n", err)
+		return "There was an issue sending the request", 500
+	}
 	if resp.StatusCode == 200 {
 		return "Successfully sent your message", 200
 	} else {
+		fmt.Println(resp.Body)
 		return "There was an issue sending the request", 500
 	}
 }
