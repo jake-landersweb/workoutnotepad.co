323a1a63df54d5d7a731522d49994bb64cef1f53
jake-landersweb <jake@landersweb.com>
2023-11-27 17:11:01 -0800

    added blog link

--- public/js/header.js
+++ public/js/header.js
@@ -6,7 +6,8 @@ Alpine.data('menu', () => ({
         { href: "/screenshots", title: "Screenshots" },
         { href: "/support", title: "Support" },
         { href: "/privacy-policy", title: "Privacy Policy" },
-        { href: "https://sapphirenw.com", title: "Our Company", external: true }
+        { href: "https://sapphirenw.com", title: "Our Company", external: true },
+        { href: "https://blog.workoutnotepad.co", title: "Blog", external: true }
     ],
 
     toggle() {
