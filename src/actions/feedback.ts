f6fca278e4dd2499045d0d4e76075a52f5f18c18
jake-landersweb <jake@landersweb.com>
2025-05-16 11:37:13 +0200

    #patch: better error reporting in feedback sender

--- src/actions/feedback.ts
+++ src/actions/feedback.ts
@@ -20,17 +20,20 @@ export async function action_SendFeedback(props: SendFeedbackProps) {
 
         const baseUrl = process.env.POCKETBASE_BASE_URL
         if (baseUrl === undefined) {
-            throw "failed to get the base url. Set the `POCKETBASE_BASE_URL` environment variable"
+            throw Error("failed to get the base url. Set the `POCKETBASE_BASE_URL` environment variable")
         }
 
-        const resp = await fetch(`${baseUrl}/api/collections/wn_feedback_v2/records`, {
+        const url = `${baseUrl}/api/collections/wn_feedback_v2/records`
+        console.log("url=", url)
+        const resp = await fetch(url, {
             method: "POST",
             headers: { 'Content-Type': 'application/json' },
             body: encoded
         })
 
         if (resp.status > 299) {
-            throw "There was an issue sending the request"
+            const text = await resp.text()
+            throw Error(`There was an issue sending the request: ${text}`)
         }
 
         console.log("successfully sent the feedback request")
